2937829390ceadd5f41774d787071577aa45a0b4
Mujung <16067529+Mujung@users.noreply.github.com>
2015-12-25 16:53:00 +0700

    style: clean up fleet traces

---
+++ internal/trace/trace.go
@@ -0,0 +1,31 @@
+// Package trace defines the provider-neutral trace record model and the
+// ingestion pipeline that reads JSONL logs, validates every field, and
+// normalizes values into a canonical form suitable for evaluation.
+//
+// A "trace" is a single recorded observation of one model handling one task:
+// how many tokens it consumed, what it cost, how long it took, how good the
+// answer was, whether it errored, and what privacy tier the data belonged to.
+// modelmariner never contacts a provider; it reasons entirely over these
+// recorded observations, which makes every run deterministic and replayable.
+package trace
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math"
+	"sort"
+	"strings"
+)
+
+// PrivacyTier classifies the sensitivity of the data a task carried. Higher
+// tiers demand stricter routing (for example, forbidding models that ship data
+// off-premises). The ordering is meaningful: Public < Internal < Confidential < Restricted.
+type PrivacyTier int
+
+const (
+	// PrivacyPublic marks data safe for any destination.
+	PrivacyPublic PrivacyTier = iota
+	// PrivacyInternal marks data that must stay within the organization.
+	PrivacyInternal
