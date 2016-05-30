bc97a7edb9e0b010c734be4a7dfd3a50dc22d928
Mujung <16067529+Mujung@users.noreply.github.com>
2016-05-30 10:00:00 +0700

    docs: document dashboard types

--- internal/reliability/reliability.go
+++ internal/reliability/reliability.go
@@ -21,3 +21,16 @@ type Aggregate struct {
 	Failures    int     `json:"failures"`
 	SuccessRate float64 `json:"success_rate"`
 
+	// Wilson lower bound gives a conservative reliability estimate that does
+	// not over-trust models with only a handful of samples.
+	WilsonLower float64 `json:"reliability_lower"`
+
+	MeanCostUSD   float64 `json:"mean_cost_usd"`
+	MeanLatencyMS float64 `json:"mean_latency_ms"`
+	P50LatencyMS  float64 `json:"p50_latency_ms"`
+	P95LatencyMS  float64 `json:"p95_latency_ms"`
+	P99LatencyMS  float64 `json:"p99_latency_ms"`
+	MeanQuality   float64 `json:"mean_quality"`
+	MeanTokens    float64 `json:"mean_tokens"`
+
+	// HighestPrivacy is the strictest privacy tier ever observed for this task,
