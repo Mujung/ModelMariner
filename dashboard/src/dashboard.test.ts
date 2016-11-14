7719b0eeb9507f197ed6b6417d6fe27c1df400fb
Mujung <16067529+Mujung@users.noreply.github.com>
2016-11-14 17:32:00 +0700

    docs: document routing weights

--- dashboard/src/dashboard.test.ts
+++ dashboard/src/dashboard.test.ts
@@ -14,3 +14,15 @@ function sampleReport(): Report {
     schema: "modelmariner/v1",
     input: {
       total_lines: 4,
+      accepted: 4,
+      rejected: 0,
+      models: ["cheap", "premium"],
+      tasks: ["t"],
+    },
+    reliability: [
+      {
+        model: "cheap", task: "t", samples: 2, successes: 2, failures: 0,
+        success_rate: 1, reliability_lower: 0.34, mean_cost_usd: 0.1,
+        mean_latency_ms: 110, p50_latency_ms: 110, p95_latency_ms: 120,
+        p99_latency_ms: 120, mean_quality: 0.81, mean_tokens: 20,
+        highest_privacy: "public",
