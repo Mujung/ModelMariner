68d4291b84e3f824f446ca28c1380b3cefd4f324
Mujung <16067529+Mujung@users.noreply.github.com>
2018-02-25 10:21:00 +0700

    fix: correct reliability tiers

--- cmd/modelmariner/main_test.go
+++ cmd/modelmariner/main_test.go
@@ -5,3 +5,16 @@ import (
 	"path/filepath"
 	"testing"
 )
+
+func writeTemp(t *testing.T, name, content string) string {
+	t.Helper()
+	dir := t.TempDir()
+	p := filepath.Join(dir, name)
+	if err := os.WriteFile(p, []byte(content), 0o644); err != nil {
+		t.Fatal(err)
+	}
+	return p
+}
+
+const traceLines = `{"model":"cheap","task":"t","tokens":{"prompt":10,"completion":10},"cost_usd":0.10,"latency_ms":100,"quality":0.80,"error":false,"privacy":"public"}
+{"model":"premium","task":"t","tokens":{"prompt":10,"completion":10},"cost_usd":0.60,"latency_ms":400,"quality":0.98,"error":false,"privacy":"public"}`
