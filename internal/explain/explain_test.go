c280007913e0f03121ff1c0e6177172f98b97410
Mujung <16067529+Mujung@users.noreply.github.com>
2018-03-20 16:33:00 +0700

    feat: add model routing

--- internal/explain/explain_test.go
+++ internal/explain/explain_test.go
@@ -4,3 +4,13 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/Mujung/modelmariner/internal/policy"
+	"github.com/Mujung/modelmariner/internal/routing"
+)
+
+func TestExplainWinner(t *testing.T) {
+	d := routing.Decision{
+		Task: "t", Policy: "p", Winner: "premium", Runnerup: "cheap",
+		Score: 0.9, Margin: 0.2,
+		Evaluations: []policy.Evaluation{
+			{Model: "premium", Eligible: true, Score: 0.9, Components: map[string]float64{"quality": 0.9}},
