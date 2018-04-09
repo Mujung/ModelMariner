86e89ce987f27983a4630a0c4d03cc066cf3eb9e
Mujung <16067529+Mujung@users.noreply.github.com>
2018-04-09 18:21:00 +0700

    perf: improve dashboard types

--- internal/pareto/pareto_test.go
+++ internal/pareto/pareto_test.go
@@ -6,3 +6,17 @@ import (
 	"github.com/Mujung/modelmariner/internal/reliability"
 )
 
+func agg(model string, cost, p95, qual, rel float64) reliability.Aggregate {
+	return reliability.Aggregate{
+		Model: model, Task: "t", Successes: 10, Samples: 10,
+		MeanCostUSD: cost, P95LatencyMS: p95, MeanQuality: qual, WilsonLower: rel,
+	}
+}
+
+func TestDominatedPointRemoved(t *testing.T) {
+	// "cheap" dominates "worse" on every axis; "premium" trades cost for quality.
+	sum := reliability.Summary{
+		Tasks: []string{"t"},
+		Aggregates: []reliability.Aggregate{
+			agg("cheap", 0.10, 100, 0.90, 0.95),
+			agg("worse", 0.20, 200, 0.80, 0.90),   // dominated by cheap
