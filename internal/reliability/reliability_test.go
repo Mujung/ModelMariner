8666b4188c313f92035c734d7686b4dd7f182900
Mujung <16067529+Mujung@users.noreply.github.com>
2018-04-30 10:21:00 +0700

    perf: improve report rendering

--- internal/reliability/reliability_test.go
+++ internal/reliability/reliability_test.go
@@ -7,3 +7,18 @@ import (
 	"github.com/Mujung/modelmariner/internal/trace"
 )
 
+func mk(model, task string, cost, lat, qual float64, err bool, priv trace.PrivacyTier) trace.Trace {
+	return trace.Trace{
+		Model: model, Task: task, CostUSD: cost, LatencyMS: lat,
+		Quality: qual, Error: err, Privacy: priv, Completion: 10, Prompt: 10,
+	}
+}
+
+func TestComputeBasic(t *testing.T) {
+	traces := []trace.Trace{
+		mk("a", "t1", 0.10, 100, 0.9, false, trace.PrivacyPublic),
+		mk("a", "t1", 0.20, 200, 0.8, false, trace.PrivacyInternal),
+		mk("a", "t1", 0.00, 0, 0, true, trace.PrivacyPublic),
+	}
+	traces[2].ErrorKind = "timeout"
+	sum := Compute(traces)
