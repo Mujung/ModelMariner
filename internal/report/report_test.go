2ef95a5ab07392f7ab3a7ff7b69e591f7d021ccf
Mujung <16067529+Mujung@users.noreply.github.com>
2018-05-10 18:15:00 +0700

    fix: correct trace sampling

---
+++ internal/report/report_test.go
@@ -0,0 +1,10 @@
+package report
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/Mujung/modelmariner/internal/pareto"
+	"github.com/Mujung/modelmariner/internal/policy"
