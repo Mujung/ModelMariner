15c068642eea1c35ac0f074483edf7ea7ac633ca
Mujung <16067529+Mujung@users.noreply.github.com>
2018-06-02 16:27:00 +0700

    docs: expand edge pruning

--- internal/report/report_test.go
+++ internal/report/report_test.go
@@ -8,3 +8,18 @@ import (
 
 	"github.com/Mujung/modelmariner/internal/pareto"
 	"github.com/Mujung/modelmariner/internal/policy"
+	"github.com/Mujung/modelmariner/internal/reliability"
+	"github.com/Mujung/modelmariner/internal/routing"
+	"github.com/Mujung/modelmariner/internal/trace"
+)
+
+const corpus = `{"model":"cheap","task":"t","tokens":{"prompt":10,"completion":10},"cost_usd":0.10,"latency_ms":100,"quality":0.80,"error":false,"privacy":"public"}
+{"model":"cheap","task":"t","tokens":{"prompt":10,"completion":10},"cost_usd":0.10,"latency_ms":120,"quality":0.82,"error":false,"privacy":"public"}
+{"model":"premium","task":"t","tokens":{"prompt":10,"completion":10},"cost_usd":0.60,"latency_ms":400,"quality":0.98,"error":false,"privacy":"public"}
+{"model":"premium","task":"t","tokens":{"prompt":10,"completion":10},"cost_usd":0.60,"latency_ms":420,"quality":0.97,"error":false,"privacy":"public"}`
+
+const policyJSON = `{"policies":[{"name":"quality","preference":{"weights":[{"objective":"quality","weight":1}]}}]}`
+
+func buildReport(t *testing.T) Report {
+	t.Helper()
+	ing, err := trace.Ingest(strings.NewReader(corpus), trace.DefaultOptions())
