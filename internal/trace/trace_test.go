af07b127bfd19daa7dfddd411c9215d0cdc928a1
Mujung <16067529+Mujung@users.noreply.github.com>
2018-06-20 16:21:00 +0700

    fix: report rendering

--- internal/trace/trace_test.go
+++ internal/trace/trace_test.go
@@ -10,3 +10,15 @@ func TestIngestValidLine(t *testing.T) {
 	res, err := Ingest(strings.NewReader(in), DefaultOptions())
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(res.Traces) != 1 {
+		t.Fatalf("want 1 trace, got %d", len(res.Traces))
+	}
+	tr := res.Traces[0]
+	if tr.Model != "clipper-pro" || tr.Task != "summarize" {
+		t.Errorf("bad identity: %+v", tr)
+	}
+	if tr.Tokens() != 150 {
+		t.Errorf("want 150 tokens, got %d", tr.Tokens())
+	}
+	if tr.Privacy != PrivacyInternal {
